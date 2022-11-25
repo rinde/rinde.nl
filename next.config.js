71fd1b082b704936f4bfb61420480e3dbc3d4ca6
Rinde van Lon <rindevanlon@gmail.com>
2022-11-25 17:58:51 +0800

    clean out nextjs

--- next.config.js
+++
@@ -1,8 +0,0 @@
-const isProd = (process.env.NODE_ENV || 'production') === 'production'
-console.log("IS PROD", isProd);
-module.exports = {
-  exportPathMap: () => ({
-    '/': { page: '/' },
-  }),
-  assetPrefix: isProd ? '' : '',
-}
